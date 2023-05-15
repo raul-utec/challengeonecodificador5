f8263161bc369439db182a28f66529e157d41a55
Raul Rivera <riveril1234@gmail.com>
2023-05-15 02:16:58 -0500

    terminado

--- script.js
+++ script.js
@@ -9,9 +9,28 @@ function encriptar(stringEncriptada){
             stringEncriptada=stringEncriptada.replaceAll(matrizcodigo[i][0],matrizcodigo[i][1])    
         }
     }
+    return stringEncriptada
 }
+
 
 function btnEncriptar(){
     const textoEncriptado=encriptar(textArea.value)
-    mensaje.value=textoEncriptado;  
+    mensaje.value=textoEncriptado;
+    textArea.value="";
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizcodigo=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptada=stringDesencriptada.toLowerCase()
+    for(let i=0;i<matrizcodigo.length;i++){
+        if (stringDesencriptada.includes(matrizcodigo[i][0])){
+            stringDesencriptada=stringDesencriptada.replaceAll(matrizcodigo[i][1],matrizcodigo[i][0])    
+        }
+    }
+    return stringDesencriptada
+}
+function btnDesencriptar(){
+    const textoDesencriptado=desencriptar(textArea.value)
+    mensaje.value=textoDesencriptado;
+    textArea.value="";
 }
